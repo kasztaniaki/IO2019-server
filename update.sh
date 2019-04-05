65bf872de26522b0e9d7a9d1baedc979b32024c7
Jakub Posłuszny <revanjjp@gmail.com>
2019-04-05 23:31:30 +0200

    Update update.sh
    Updated shell script for automatization (no user confirmation needed for updating evironment)

--- update.sh
+++ update.sh
@@ -4,21 +4,21 @@ echo ADDING conda-forge channel AS lower priority:
 conda config --append channels conda-forge
 
 echo INSTALLING black:
-conda install --name IO2019-server -c conda-forge black
+conda install -y --name IO2019-server -c conda-forge black
 
 echo REMOVING pymysql:
-conda remove --name IO2019-server pymysql
+conda remove -y  --name IO2019-server pymysql
 
 echo INSTALLING sqlite:
-conda install --name IO2019-server sqlite
+conda install -y  --name IO2019-server sqlite
 
 echo REMOVING unnecessary packages:
-conda remove --name IO2019-server pycparser
-conda remove --name IO2019-server idna
-conda remove --name IO2019-server cryptography
-conda remove --name IO2019-server cffi
-conda remove --name IO2019-server asn1crypto
+conda remove -y --name IO2019-server pycparser
+conda remove -y --name IO2019-server idna
+conda remove -y --name IO2019-server cryptography
+conda remove -y --name IO2019-server cffi
+conda remove -y --name IO2019-server asn1crypto
 
 echo UPDATING conda:
-conda update -n base -c defaults conda
+conda update -y -n base -c defaults conda
 # for people who don't have black in their environment
