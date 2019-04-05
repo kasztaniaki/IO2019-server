265d8be362134ac40313d8a4fcea402535bbd01e
Jakub Posłuszny <revanjjp@gmail.com>
2019-04-05 10:15:30 +0200

    Environment update
    Updating environment for SQLite, removing MySQL.

--- update.sh
+++ update.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-conda install -c conda-forge black
+echo ADDING conda-forge channel AS lower priority:
+conda config --append channels conda-forge
 
+echo INSTALLING black:
+conda install --name IO2019-server -c conda-forge black
+
+echo REMOVING pymysql:
+conda remove --name IO2019-server pymysql
+
+echo INSTALLING sqlite:
+conda install --name IO2019-server sqlite
+
+echo REMOVING unnecessary packages:
+conda remove --name IO2019-server pycparser
+conda remove --name IO2019-server idna
+conda remove --name IO2019-server cryptography
+conda remove --name IO2019-server cffi
+conda remove --name IO2019-server asn1crypto
+
+echo UPDATING conda:
+conda update -n base -c defaults conda
 # for people who don't have black in their environment
