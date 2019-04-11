dc542c0c438a48db4d2172d68e8de72396cda8e9
pulnar <agnieszka.pulnar@nokia.com>
2019-04-11 15:16:30 +0200

    Add Pool representation

---
+++ main/models.py
@@ -0,0 +1,14 @@
+from flask_appbuilder import Model
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date
+
+
+class Pool(Model):
+    pool_id = Column(Integer, primary_key=True)
+    display_name = Column(String(150), unique=True, nullable=False)
+    maximum_count = Column(Integer)
+    enabled = Column(Boolean)
+    description = Column(String(150), unique=True, nullable=False)
+    OS = Column(String(150), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.name
