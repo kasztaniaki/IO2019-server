6064f9024be1c62f27bceb1e9c592470276c1344
pulnara <magnes.agnieszka@gmail.com>
2019-04-11 18:07:36 +0200

    Update Pool model

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
@@ -9,4 +13,7 @@ def hello_world():
 
 
 if __name__ == "__main__":
+    app.config.from_object(Config)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
     app.run()
--- main/models.py
+++ main/models.py
@@ -1,14 +1,16 @@
-from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date
+from app import db
 
 
-class Pool(Model):
-    pool_id = Column(Integer, primary_key=True)
-    display_name = Column(String(150), unique=True, nullable=False)
-    available_count = Column(Integer)
-    enabled = Column(Boolean)
-    description = Column(String(500), unique=True, nullable=False)
-    OS = Column(String(150), unique=True, nullable=False)
+class Pool(db.Model):
+    pool_id = db.Column(db.Integer, primary_key=True)
+    display_name = db.Column(db.String(150), unique=True, nullable=False)
+    available_count = db.Column(db.Integer)
+    enabled = db.Column(db.Boolean)
+    description = db.Column(db.String(500), unique=True, nullable=False)
+    OS = db.Column(db.String(150), unique=True, nullable=False)
+
+    def __init__(self):
+        pass
 
     def __repr__(self):
         return self.name
