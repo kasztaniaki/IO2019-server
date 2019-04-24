af4d76a8e9d59977b36687d0c28db37336df82da
pulnara <magnes.agnieszka@gmail.com>
2019-04-24 18:34:11 +0200

    Improve parsing

--- parser/csvparser.py
+++ parser/csvparser.py
@@ -23,9 +23,9 @@ class Parser:
                 pool_description = ""
                 os_language = "PL/EN"
                 os_version = ""
-                row[4].replace(" oraz ", ",")
-                row[4].replace(" i ", ",")
-                raw_software_list = row[4].split(",")
+                row[4].replace(" oraz ", " , ")
+                row[4].replace(" i ", " , ")
+                raw_software_list = row[4].split(" , ")
 
                 pool_id = Pool.add_pool(
                     pool_name,
