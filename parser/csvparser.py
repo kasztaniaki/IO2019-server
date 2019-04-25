285759f5c630072619e8486fe8f16e8b423d11cb
Jakub Posłuszny <revanjjp@gmail.com>
2019-04-25 14:30:37 +0200

    Update csvparser.py
    Better csv error handling

--- parser/csvparser.py
+++ parser/csvparser.py
@@ -11,13 +11,24 @@ class Parser:
         csv_string = str(self.file.read(), "utf-8")
         csv_reader = csv.reader(csv_string.split("\n"), delimiter=",")
         line_count = 0
+
         for row in csv_reader:
             if line_count == 0:
                 line_count += 1
             else:
                 pool_name = row[0]
-                pool_display_name = row[1][0 : row[1].find("(") - 1]
-                os_name = row[1][row[1].find("(") + 1 : row[1].find(")")]
+
+                os_start = row[1].find("(")
+                os_end = row[1].find(")")
+
+                # chcecking if OS name is provided in
+                if os_start < 0 or os_end < 0:
+                    os_name = " - "
+                    pool_display_name = row[1]
+                else:
+                    os_name = row[1][os_start + 1 : os_end]
+                    pool_display_name = row[1][0 : os_start - 1]
+
                 pool_maximumcount = row[2]
                 enabled = row[3]
                 pool_description = ""
@@ -32,13 +43,30 @@ class Parser:
                     pool_description,
                 )
 
-                # row[4].replace(" oraz ", " , ")
-                # row[4].replace(" i ", " , ")
-                raw_software_list = row[4].split(',')
+                if pool_id < 0:
+                    return -1
+
+                raw_software_list = row[4].split(",")
 
                 for line in raw_software_list:
-                    software_name = line[0 : line.find("(") - 1].strip()
-                    software_version = line[line.find("(") + 1 : line.find(")")].strip()
-                    InstalledSoftware.add_software_to_pool(
-                        pool_id, software_name, software_version
-                    )
+                    soft_ver_start = line.find("(")
+                    soft_ver_end = line.find(")")
+                    if soft_ver_start < 0 or soft_ver_end < 0:
+                        if len(line) > 20:
+                            pool_description = pool_description + " | " + line
+                        else:
+                            InstalledSoftware.add_software_to_pool(pool_id, line, " - ")
+                    else:
+                        software_name = line[0 : soft_ver_start - 1].strip()
+                        software_version = line[
+                            soft_ver_start + 1 : soft_ver_end
+                        ].strip()
+
+                        if len(software_version) > 10:
+                            software_version = " - "
+                            
+                        InstalledSoftware.add_software_to_pool(
+                            pool_id, software_name, software_version
+                        )
+                # Pool.set_pool_description(pool_id, pool_description) - brak metody
+        return 0
